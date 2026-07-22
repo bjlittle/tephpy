932caa5c7c19bd30aa2e6e4c7f7d7b58b08faf94
Bill Little <bill.james.little@gmail.com>
2026-07-22 21:58:37 +0000

    feat: package skeleton with setuptools_scm versioning

---
+++ tests/test_import.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2026, tephpy Contributors.
+#
+# This file is part of tephpy and is distributed under the 3-Clause BSD license.
+# See the LICENSE file in the package root directory for licensing details.
+"""Smoke tests for the tephpy package skeleton."""
+
+from __future__ import annotations
+
+import tephpy
+
+
+def test_package_imports() -> None:
+    """tephpy imports and exposes a non-empty version string."""
+    assert isinstance(tephpy.__version__, str)
+    assert tephpy.__version__
+
+
+def test_version_is_pep440() -> None:
+    """The setuptools_scm version parses as a PEP 440 version."""
+    from packaging.version import Version
+
+    Version(tephpy.__version__)
